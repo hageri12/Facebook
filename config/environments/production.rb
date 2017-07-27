7ad2f382efd7eb9971b547839e69cd752b70db82
yamanishiyuhei <yuhei@yamanishiyuhei-no-MacBook-Air.local>
2017-07-28 00:26:23 +0900

    haha

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://hidden-brushlands-51646.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://13.113.133.33/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
